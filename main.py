d27d86d5bc0a3edc85bedd45e34c9f05bef226f1
Gordon Tyler <gordon@doxxx.net>
2020-04-04 22:28:24 -0400

    Add HTTP JSON endpoint which returns last state.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from RFM69 import Radio, FREQ_915MHZ
 import time
 import logging
+import json
+import asyncio
+from aiohttp import web
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
+
+current_data = {
+    "last_update": time.time(),
+    "active": False,
+}
+
+async def root(request):
+    return web.json_response(current_data)
 
 network_id = 1
 my_node_id = 1
@@ -28,11 +39,16 @@ def parse_message(data: bytes) -> Message:
     return Message(type, data[4:])
 
 def process_message(msg: Message):
+    global current_data
     if msg.type == 0x01: # Proximity State Change
         active = msg.data[0] != 0
         logging.info("Proximity State Change: active=%s", repr(active))
+        current_data = {
+            "last_update": time.time(),
+            "active": active,
+        }
 
-if __name__ == "__main__":
+async def listen_radio_packets():
     with Radio(FREQ_915MHZ, my_node_id, networkID=network_id, isHighPower=True, verbose=False) as radio:
         logging.info("Radio initialized, waiting for packets...")
 
@@ -48,10 +64,24 @@ if __name__ == "__main__":
 
             # Process any packets received in the last second
             for packet in radio.get_packets():
-                logging.debug("Received packet from #%d (RSSI=%d): %s", packet.sender, packet.RSSI, repr(packet.data))
+                logging.debug("Received packet from #%d (RSSI=%d): %s",
+                              packet.sender, packet.RSSI, repr(packet.data))
                 data = bytes(packet.data)
                 msg = parse_message(data)
                 if msg:
                     process_message(msg)
             
-            time.sleep(1)
+            await asyncio.sleep(1)
+
+async def main():
+    app = web.Application()
+    app.add_routes([web.get("/", root)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host="0.0.0.0", port=8111)
+    await site.start()
+
+    await listen_radio_packets()
+
+if __name__ == "__main__":
+    asyncio.run(main())
